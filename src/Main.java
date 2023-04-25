dfedb107316e241e9a63e8141c79244b9ebf63b1
iMandarin <odhamdhere@gmail.com>
2023-04-25 16:35:59 +0530

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,110 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Hello and welcome!");
+        char[][] board = new char[3][3];
+        for (int row = 0; row< board.length; row++)
+        {
+            for (int col = 0; col< board[row].length;col++)
+            {
+                board[row][col] = ' ';
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            }
         }
+        char player = 'X';
+        boolean gameOver = false;
+        Scanner scanner = new Scanner(System.in);
+        while (!gameOver)
+        {
+            printBoard(board);
+            System.out.println("Player " + player + " enter: ");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            if (board[row][col] == ' ')
+            {
+
+                board[row][col] = player; // place the element
+                gameOver = haveWon(board, player);
+                if (gameOver)
+                {
+                    System.out.println("Player " + player + " has won ");
+                }
+                else
+                {
+//                    if (player == 'X')
+//                    {
+//                        player == 'O';
+//                    }
+//                    else
+//                    {
+//                        player = 'X';
+//                    }
+
+                    //short one line format
+                    player = (player == 'X') ? 'O' : 'X';
+                }
+            }
+            else
+            {
+                System.out.println("Invalid Move! Try Again!!");
+            }
+
+        }
+        printBoard(board);
     }
+    public static void printBoard (char[][] board)
+    {
+        for (int row = 0; row< board.length; row++)
+        {
+            for (int col = 0; col< board[row].length;col++)
+            {
+                System.out.print(board[row][col] + " | ");
+
+
+            }
+            System.out.println( );
+        }
+
+    }
+    public static boolean haveWon(char[][] board, char player)
+    {
+        //check the rows
+        for (int row = 0; row< board.length; row++)
+        {
+            if (board[row][0] == player && board[row][1] == player && board[row][2] == player)
+            {
+                return true;
+            }
+
+            //System.out.println( );
+        }
+
+        //check col
+        for (int col = 0; col < board[0].length; col++)
+        {
+            if (board[0][col] == player && board[1][col] == player && board[2][col] == player)
+            {
+                return true;
+            }
+
+            //System.out.println( );
+        }
+
+        //diagonally
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+        {
+            return true;
+        }
+
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+        {
+            return true;
+        }
+        return false;
+
+    }
+
 }
